22a1e0f518f602a39633dfedf02ceff4accd9b28
Gaita Iustin Florian <iustingaita.f@gmail.com>
2026-02-04 14:08:33 +0200

    Initial commit

---
+++ src/test/java/com/gifprojects/membersphere/MembersphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gifprojects.membersphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MembersphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
